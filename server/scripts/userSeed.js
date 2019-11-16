56818902692d1b38c8d605ed0672741f3ee5f092
Frank Zachary <fzachary@gmail.com>
2019-11-16 12:51:21 -0500

    added user seed data for login, updated exports and imports and data syntax so we're speaking the language of breweries instead of books, changed some file names to fit our proper schema

---
+++ server/scripts/userSeed.js
@@ -0,0 +1,59 @@
+// DEPENDENCIES =====================
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// Empty the user DB and insert the information below
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/triangleontap"
+);
+
+const userSeed = [
+    {
+        firstName: "Miran",
+        lastName: "Jang",
+        username: "mjang",
+        password: db.User.hashPassword("admin"),
+        breweries: []
+    },
+    {
+        firstName: "Ryan",
+        lastName: "Kooy",
+        username: "rkooy",
+        password: db.User.hashPassword("admin"),
+        breweries: []
+    },
+    {
+        firstName: "Matthew",
+        lastName: "Parks",
+        username: "mparks",
+        password: db.User.hashPassword("admin"),
+        breweries: []
+    },
+    {
+        firstName: "Jason",
+        lastName: "Whited",
+        username: "jwhited",
+        password: db.User.hashPassword("admin"),
+        breweries: []
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zachary",
+        username: "fzachary",
+        password: db.User.hashPassword("admin"),
+        breweries: []
+    },
+];
+
+db.User
+    .remove({})
+    .then(() => db.User.collection.insertMany(userSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.err(err);
+        process.exit(1);
+    });
