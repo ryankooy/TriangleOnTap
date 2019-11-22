9782128644b21246a312eb530e32acd414601ef7
Frank Zachary <fzachary@gmail.com>
2019-11-21 20:42:13 -0500

    added some new files for the brewery API search. configured routes. validated data returned from openbrewerydb api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
 export default {
+  // Search breweries on the Openbrewerydb API
+  searchBreweries: function(query) {
+    return axios.post("/api/search", query)
+  },
   // Gets all books
   getBreweries: function() {
     return axios.get("/api/breweries");
   },
   // Gets the book with the given id
   getBrewery: function(id) {
-    return axios.get("/api/brewery/" + id);
+    return axios.get("/api/breweries/" + id);
   },
   // Deletes the book with the given id
   deleteBrewery: function(id) {
