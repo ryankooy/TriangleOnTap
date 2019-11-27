b584b65a8d13b1129cf6a89c3ec51e3bd4957145
Ryan Kooy <rwkooy@gmail.com>
2019-11-26 21:24:48 -0500

    heroku test

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3007;
 
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/triangleontap';
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://user:admin1@ds037155.mlab.com:37155/heroku_trz1rz2v';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Middlewares
 app.use(morgan('dev'));
